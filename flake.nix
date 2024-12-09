1d23598aa4c476dba21e42d59bf6939523ba835d
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2024-12-09 12:03:46 +0530

    trying to fix issue with rust analyzer

--- flake.nix
+++ flake.nix
@@ -8,7 +8,23 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
+      let
+        pkgs = import nixpkgs {
+          system = system;
+          overlays = [
+            (self: super: {
+              rust-analyzer = super.rust-analyzer.overrideAttrs (old: {
+                src = super.fetchFromGitHub {
+                  owner = "rust-lang";
+                  repo = "rust-analyzer";
+                  rev = "822644d97d7f64e1bdff25b1d636e366a29facc4";
+                  sha256 = "sha256-iJTCG7vtECll27sxDElL4SuIY/kRhamJf0DDYCW6fb4";
+                };
+              });
+            })
+          ];
+        };
+      in
       {
         devShells = {
           default = pkgs.mkShell {
